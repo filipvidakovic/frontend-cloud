9972362ea9e8db04c318c98de59781e9024032c3
DusicaPesic <130140285+DusicaPesic@users.noreply.github.com>
2025-08-19 23:48:01 +0200

    [ADD] styling and header to app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import AddArtistForm from "./components/AddArtistForm";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const handleAddArtist = (artist: {
+    firstName: string;
+    lastName: string;
+    age: number;
+    bio: string;
+    genres: string[];
+  }) => {
+    console.log("New artist added:", artist);
+    alert(`Artist ${artist.firstName} ${artist.lastName} added!`);
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <header className="app-header">
+        <h1 className="display-3">MySpotify</h1>
+        <p>Your music, your way.</p>
+      </header>
+
+      <div className="form-container">
+        <AddArtistForm onSubmit={handleAddArtist} />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
