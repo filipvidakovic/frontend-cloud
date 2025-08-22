d3bf075bcd4c9f8679c79e2585a8510a103f5556
DusicaPesic <130140285+DusicaPesic@users.noreply.github.com>
2025-08-22 22:21:04 +0200

    [ADD] ApiService for calling backend

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,26 @@
 import { useState } from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import "./App.css";
-import AddArtistForm from "./components/AddArtistForm";
-
+import AddArtistForm from "./components/artist/AddArtistForm";
+import type { AlbumCardProps } from "./models/Album";
+import type { ArtistCardProps } from "./models/Artist";
+import AlbumList from "./components/album/AlbumList";
+import ArtistList from "./components/artist/ArtistList";
+import DiscoverPage from "./pages/DiscoverPage";
+import Header from "./components/header/Header";
 function App() {
+  const staticAlbums: AlbumCardProps[] = [
+    { albumId: "ALB-001", genre: "Rock", titleList: ["Song A", "Song B"] },
+    { albumId: "ALB-002", genre: "Jazz", titleList: ["Track 1", "Track 2"] },
+    { albumId: "ALB-003", genre: "Pop", titleList: ["Hit Single"] },
+  ];
+
+  const staticArtists: ArtistCardProps[] = [
+    { artistId: "ART-001", genres: ["Rock", "Blues"] },
+    { artistId: "ART-002", genres: ["Jazz"] },
+    { artistId: "ART-003", genres: ["Pop", "Dance"] },
+  ];
+
   const handleAddArtist = (artist: {
     firstName: string;
     lastName: string;
@@ -15,16 +33,15 @@ function App() {
   };
 
   return (
-    <>
-      <header className="app-header">
-        <h1 className="display-3">MySpotify</h1>
-        <p>Your music, your way.</p>
-      </header>
+    <Router>
+      <Header></Header>
 
-      <div className="form-container">
-        <AddArtistForm onSubmit={handleAddArtist} />
-      </div>
-    </>
+      <Routes>
+        <Route path="/" />
+
+        <Route path="/discover" element={<DiscoverPage />} />
+      </Routes>
+    </Router>
   );
 }
 
